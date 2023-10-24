d3b237b0b46e03dd1528e5f8feec3b4f5b55110b
MaksimMyson <maksimmyson33@gmail.com>
2023-10-24 20:28:00 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
+	double lenth = 0.0;
+	
+
+	cout << "Enter the lenth of the circle: ";
+	cin >> lenth;
+	double R2 = lenth / (2 * 3.14);
+	cout << "The radius of the circle is: " << R2 << endl;
+	double S = 3.14 * R2;
+	cout << "The area of the circle is: " << S << endl;
 	
 	return 0;
 }
