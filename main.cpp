abef665855e3ef85762802bb8589e5ea76ab6dbc
MaksimMyson <maksimmyson33@gmail.com>
2023-10-24 20:13:03 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	
+int main() {
+	double R1, R2, R3;
+	cout << "value R1:";
+	cin >> R1;
+	cout << "value R2:";
+	cin >> R2;
+	cout << "value R3:";
+	cin >> R3;
+
+	double R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+	cout << "R0 = " << R0 << endl;
+
 	return 0;
 }
+
+
+
+
