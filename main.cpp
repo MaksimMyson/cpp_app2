b32bc3e0ba2ee6f2a2068b4d14e531f32dc0c402
MaksimMyson <maksimmyson33@gmail.com>
2023-10-24 20:33:43 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	
-	return 0;
+int main() {
+    double v, t, a;
+
+    cout << "Enter velocity (v): ";
+    cin >> v;
+
+    cout << "Enter time (t): ";
+    cin >> t;
+
+    cout << "Enter acceleration (a): ";
+    cin >> a;
+
+    double distance = v * t + (a * t * t) / 2;
+
+    cout << "Distance traveled: " << distance << " units." << endl;
+
+    return 0;
 }
