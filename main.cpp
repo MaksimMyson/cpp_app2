17b5f6e2f888e9dee7e7488e11281be71f0cff24
MaksimMyson <maksimmyson33@gmail.com>
2023-10-24 20:15:25 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
 	double R1, R2, R3;
-	cout << "value R1:";
+	cout << "Enter R1:";
 	cin >> R1;
-	cout << "value R2:";
+	cout << "Enter R2:";
 	cin >> R2;
-	cout << "value R3:";
+	cout << "Enter R3:";
 	cin >> R3;
 
 	double R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
